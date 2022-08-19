a2115f0bd4630cc5565e9863d0a5d2be863ce903
EngJun <109129614+EngJun@users.noreply.github.com>
2022-08-19 09:29:35 +0800

    Update register.js

--- public/js/register.js
+++ public/js/register.js
@@ -1,6 +1,6 @@
 function registerMe(){
     var registerUser = new XMLHttpRequest();
-    registerUser.open("POST","http://127.0.0.1:8080/addProfile", true);
+    registerUser.open("POST","18.212.8.244:3000/addProfile", true);
     registerUser.setRequestHeader("Content-Type", "application/json");
     registerUser.onload=function(){
         console.log("ok");
